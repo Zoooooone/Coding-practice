314597d2c608fcaeb2f68871185e9d03f5bf010f
Zoooooone <opitz4546@gmail.com>
2024-04-07 20:07:20 +0900

    feat: update maximum concatenate length

--- algorithm/string/maximum_concatenate_length.py
+++ algorithm/string/maximum_concatenate_length.py
@@ -1,7 +1,25 @@
 from collections import defaultdict
 
 
-def f(strings):
+def maximumConcatenateLength_brute_force(strings):
+    ans = 0
+    prefix_table = defaultdict(list)
+
+    for i, s in enumerate(strings):
+        prefix = s[:2]
+        prefix_table[prefix].append(i)
+
+    for s in strings:
+        suffix = s[-2:]
+        if suffix in prefix_table:
+            for i in prefix_table[suffix]:
+                if strings[i] != s:
+                    ans = max(ans, len(strings[i]) + len(s) - 2)
+
+    return ans
+
+
+def maximumConcatenateLength(strings):
     prefix_table, suffix_table, common_table = defaultdict(int), defaultdict(int), defaultdict(int)
     ans = 0
 
@@ -35,4 +53,5 @@ if __name__ == "__main__":
         "kjkjkjkjkj"
     ]
 
-    print(f(strings))
+    print(maximumConcatenateLength(strings))
+    print(maximumConcatenateLength_brute_force(strings))
